a963662c450d411f829ceea8989c76dd9fc2a536
Sonins <gmlrhks95@gmail.com>
2021-10-31 11:29:36 +0900

    Lidar.py : path delimiter to os.sep

--- LiDAR.py
+++ LiDAR.py
@@ -1,6 +1,7 @@
 import inspect
 import sys
 import platform
+import os
 
 from Authority import AuthorityExecption
 
@@ -15,23 +16,11 @@ class LiDAR:
 
     @data.setter
     def data(self, new_data):
-        if platform.system() == 'Windows':
-            if inspect.stack()[1][1].split('\\')[-1] == 'Game.py':
-                self.__data = new_data
-            else:
-                sys.tracebacklimit = 0
-                raise AuthorityExecption('Not allowed File %s is trying to \
-                    change LiDAR.data at \'%s\'' % (
-                            inspect.stack()[1][1].split('\\')[-1],
-                            inspect.stack()[1][0])
-                        )
+        if inspect.stack()[1][1].split(os.sep)[-1] == 'Game.py':
+            self.__data = new_data
         else:
-            if inspect.stack()[1][1].split('/')[-1] == 'Game.py':
-                self.__data = new_data
-            else:
-                sys.tracebacklimit = 0
-                raise AuthorityExecption('Not allowed File %s is trying to \
-                    change LiDAR.data at \'%s\'' % (
-                            inspect.stack()[1][1].split('/')[-1],
-                            inspect.stack()[1][0])
-                        )
+            sys.tracebacklimit = 0
+            raise AuthorityExecption('Not allowed File %s is trying to \
+                change LiDAR.data at \'%s\'' % (
+                        inspect.stack()[1][1].split(os.sep)[-1],
+                        inspect.stack()[1][0]))
