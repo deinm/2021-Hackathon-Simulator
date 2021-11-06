d5b559a2316d9863862f391546f36774cce4a4ca
jungjh404 <jungjh404@gmail.com>
2021-11-06 20:24:38 +0900

    dynamic wall complete

--- Wall.py
+++ Wall.py
@@ -18,29 +18,34 @@ class WallSprite(pygame.sprite.Sprite):
         pass
 
 class DynamicWallSprite(pygame.sprite.Sprite):
-    def __init__(self, position, width, height):
+    def __init__(self, initial_position, target_position, width, height):
         super(DynamicWallSprite, self).__init__()
         black_wall = 255 * np.ones((width, height, 3))
         self.normal = pygame.surfarray.make_surface(black_wall)
         self.rect = pygame.Rect(self.normal.get_rect())
-        self.initial_position = position
-        self.rect.center = position
+        self.initial_position = initial_position
+        self.target_position = target_position
+        self.rect.center = self.initial_position
         self.image = self.normal
         self.time = time.time()
 
     def update(self):
         elpased_time = (time.time() - self.time)
-        tik = (int)(elpased_time * 100) % 100
-        if  (int)(elpased_time % 4) == 0:
+        tik = (int)(elpased_time * 100) % 200
+
+        if  (int)(elpased_time % 8) < 2:
             self.rect.center = self.initial_position
-        if  (int)(elpased_time % 4) == 2: 
-            self.rect.center = (600,600)    
-        if  (int)(elpased_time % 4) == 1:
-            print("move")
-            self.rect.center = (500+tik,500+tik)
-        if  (int)(elpased_time % 4) == 3:
-            print("move")
-            self.rect.center = (600-tik, 600-tik)
+
+        if  4<= (int)(elpased_time % 8) < 6: 
+            self.rect.center = self.target_position    
+
+        elif  2<=(int)(elpased_time % 8) < 4:
+            self.rect.center = (self.initial_position[0] + (int)((self.target_position[0] - self.initial_position[0])*tik/200),\
+                                 self.initial_position[1] + (int)((self.target_position[1] - self.initial_position[1])*tik/200))
+
+        elif  6<=(int)(elpased_time % 8) < 8:
+            self.rect.center = (self.target_position[0] - (int)((self.target_position[0] - self.initial_position[0])*tik/200),\
+                                 self.target_position[1] - (int)((self.target_position[1] - self.initial_position[1])*tik/200))
         
 
 
